b23a82c40076aa7620b59096b24d2879878f7ace
Oleksandr Boichenko <oleksandr.boichenko@clockwise.software>
2025-08-28 11:41:08 +0300

    Update Docker configuration to change application port from 3000 to 4000; refactor main.ts to dynamically retrieve port from environment variables. Enhance startup logging to indicate the running port.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+  const port = configService.getOrThrow<number>("PORT");
+
   // Enable CORS for GraphQL Playground
   app.enableCors();
 
-  await app.listen(4000);
+  await app.listen(port);
+  console.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
 void bootstrap();
